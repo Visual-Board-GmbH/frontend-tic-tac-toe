df3a01ac9dc2a4b2a5f0611a1b7f363aab35bed6
Samuel Salomon <samuel.salomon@b-rightsource.com>
2020-07-15 08:56:51 +0200

    added the navigation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar.vue"
+import Vue from 'vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import VueRouter from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+Vue.use(VueRouter)
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
-import App from './App.vue'
+import './plugins/bootstrap-vue'
+import VueRouter from "vue-router";
+import App from "./App"
+import PlayView from "./views/PlayView"
+import UserView from "./views/UserView";
+import HistoryView from "./views/HistoryView";
 
 Vue.config.productionTip = false
 
+
+
+const routes = [
+  { path: '/play', component: PlayView },
+  { path: '/history', component: UserView },
+  { path: '/user', component: HistoryView }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+});
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
